79ce50eb1a3022e6d4c76d03c7431d95b66542aa
DESKTOP-DFLEMQ3\pc <chamseddine.bezzaouia@gmail.com>
2020-05-02 04:43:59 +0100

    affiliation Admin

--- src/Controller/Api/ApiAffiliationController.php
+++ src/Controller/Api/ApiAffiliationController.php
@@ -49,8 +49,6 @@ class ApiAffiliationController extends AbstractController
             true
         );
 
-
-
         $username = $data['username'];
         $password = $data['password'];
         $email = $data['email'];
@@ -86,13 +84,8 @@ class ApiAffiliationController extends AbstractController
             if ($withcoupon == "1"){
 
                 $couponn =$dm->getRepository(Coupon::class)->findOneBy(array('code'=>$coupon));
-
                 $upline = $couponn->getOwner();
-
-                
-
             }
-
 
             $user->setUpline($upline);
 
@@ -114,15 +107,109 @@ class ApiAffiliationController extends AbstractController
         $dm->flush();
 
 
-        /*$partnership->setCodeDownline($user->getCodeUser());
 
-        $partnership->setCodeUpline($codeupline);
-        $dm->persist($partnership);
-        $dm->flush();*/
 
 
         return new JsonResponse(["success" => $user->getUsername(). " has been registered!"], 200);
 
 
     }
+
+
+    ///////////////////////////////////////////////////////
+    ///////////  ADD USER AFFILIATION TO ADMIN ////////////
+    ///////////////////////////////////////////////////////
+
+    /**
+     * @Route( "/new, name="api_user_affiliation", methods={"POST"})
+     * @param User $user
+     * @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function newToAdminAffiliation(SerializerInterface $serializer, Request $request,  UserManagerInterface $userManager,DocumentManager  $dm){
+
+
+
+
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+
+        $username = $data['username'];
+        $password = $data['password'];
+        $email = $data['email'];
+        $codeuser = substr(md5(time()), 0, 15);
+        $coupon = $data['coupon'];
+        $withcoupon = $data['withcoupon'];
+        $firstname = $data['firstname'];
+        $lastname = $data['lastname'];
+        $address = $data['address'];
+        $postalcode = $data['postalcode'];
+        $city = $data['city'];
+        $country = $data['country'];
+
+
+
+        $user = new User();
+        $user->setUsername($username);
+        $user  ->setPlainPassword($password);
+        $user->setEmail($email);
+        $user->setEnabled(true);
+        $user->setRoles(array('ROLE_USER'));
+        $user->setSuperAdmin(false);
+        $user->setCodeUser($codeuser);
+        $user->setLevel('0');
+        $user->setPartnership($partnership);
+        $user->setFirstname($firstname);
+        $user->setLastname($lastname);
+        $user->setAddress($address);
+        $user->setPostalcode($postalcode);
+        $user->setCity($city);
+        $user->setCountry($country);
+
+        if ($withcoupon == "1"){
+
+            $couponn =$dm->getRepository(Coupon::class)->findOneBy(array('code'=>$coupon));
+            $upline = $couponn->getOwner();
+        }
+        if ($withcoupon == "0") {
+
+            $couponn =$dm->getRepository(Coupon::class)->findOneBy(array('code'=>'BitLeadPro'));
+            $upline = $couponn->getOwner();
+        }
+
+        $user->setUpline($upline);
+
+
+        try {
+            $userManager->updateUser($user, true);
+        } catch (\Exception $e) {
+            return new JsonResponse(["error" => $e->getMessage()], 500);
+        }
+
+        $couponObject = new Coupon();
+        $couponObject->setName('Affiliation');
+        $couponObject->setActive('1');
+        $couponObject->setType('starter');
+        $couponObject->setValue(0);
+        $couponObject->setCode($user->getUsername());
+        $couponObject->setOwner($user);
+        $dm->persist($couponObject);
+        $dm->flush();
+
+
+
+
+
+        return new JsonResponse(["success" => $user->getUsername(). " has been registered!"], 200);
+
+
+    }
+
+
+
+
+
+
+
 }
